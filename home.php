8718fcbb5240937814dfed217b0be6403e961900
ricky-hrf <riskiharefa2000@gmail.com>
2024-02-23 00:14:44 +0700

    menambahkan animasi

--- home.php
+++ home.php
@@ -102,6 +102,7 @@ if (isset($_POST["cari"])) {
   <section>
     <a href="logout.php" class="logout">
       <=Keluar </a>
+
   </section>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -7,6 +7,8 @@
   <title>My Portofolio</title>
   <!-- bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- AOS -->
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
   <!-- my css -->
   <link rel="stylesheet" href="css/style.css">
   <!-- icon -->
@@ -52,7 +54,7 @@
   <section class="jumbotron text-center">
     <img src="asset/gambar/profil.jpg" alt="profil" width="200" class="rounded-circle img-thumbnail">
     <h1 class="display-4 fw-bold">Ricky Harefa</h1>
-    <p class="lead">I'm a Developer & designer</p>
+    <p class="lead"></p>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
       <path fill="#ffffff" fill-opacity="1" d="M0,0L60,26.7C120,53,240,107,360,112C480,117,600,75,720,90.7C840,107,960,181,1080,181.3C1200,181,1320,107,1380,69.3L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
     </svg>
@@ -68,10 +70,10 @@
         </div>
       </div>
       <div class="row justify-content-center fs-5 text-center">
-        <div class="col-md-4 mb-3">
+        <div class="col-md-4 mb-3" data-aos="fade-right" data-aos-duration="1000">
           <p>Hai, Saya Ricky Harefa, seorang mahasiswa informatika dan penikmat seni koding. Dengan semangat petualangan, saya berupaya menciptakan pengalaman online yang tak terlupakan</p>
         </div>
-        <div class="col-md-4 mb-3">
+        <div class="col-md-4 mb-3" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="200">
           <p>Saya tidak pernah berhenti berimajinasi, selalu siap menghadirkan inovasi dalam dunia desain web. Bersiaplah untuk menjelajahi bersama saya ke dimensi baru di dunia digital yang tak terbatas!</p>
         </div>
       </div>
@@ -92,7 +94,7 @@
       </div>
       <div class="row justify-content-evenly">
         <div class="col-md-4 mb-3">
-          <div class="card">
+          <div class="card" data-aos="flip-left">
             <a href="buku.php" for="buku">
               <img src="asset/gambar/1.jpg" height="250" class="card-img-top" alt="project1">
             </a>
@@ -102,7 +104,7 @@
           </div>
         </div>
         <div class="col-md-4 mb-3">
-          <div class="card">
+          <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="100">
             <a href="machine.php">
               <img src="asset/gambar/2.jpg" height="250" class="card-img-top" alt="project2">
             </a>
@@ -112,7 +114,7 @@
           </div>
         </div>
         <div class="col-md-4 mb-3">
-          <div class="card">
+          <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="200">
             <a href="deep.php">
               <img src="asset/gambar/5.jpg" height="250" class="card-img-top" alt="project3">
             </a>
@@ -122,7 +124,7 @@
           </div>
         </div>
         <div class="col-md-4 mb-3">
-          <div class="card">
+          <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="300">
             <a href="jst.php">
               <img src="asset/gambar/4.jpg" height="250" class="card-img-top" alt="project4">
             </a>
@@ -132,7 +134,7 @@
           </div>
         </div>
         <div class="col-md-4 mb-3">
-          <div class="card">
+          <div class="card" data-aos="flip-left" data-aos-duration="500" data-aos-delay="400">
             <a href="mobile.php">
               <img src="asset/gambar/3.jpg" height="250" class="card-img-top" alt="project5">
             </a>
@@ -200,6 +202,42 @@
   <!-- akhir footer -->
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+  <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+  <script>
+    AOS.init({
+      once: true, // whether animation should happen only once - while scrolling down
+    });
+  </script>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/TextPlugin.min.js"></script>
+  <script>
+    gsap.registerPlugin(TextPlugin);
+    gsap.to('.lead', {
+      duration: 2,
+      delay: 1.5,
+      text: ' I am a Developer & designer'
+    });
+    gsap.from('.jumbotron img', {
+      duration: 1,
+      rotateY: 360,
+      opacity: 0
+    });
+    gsap.from('.navbar', {
+      duration: 1.5,
+      y: '-100%',
+      opacity: 0,
+      ease: 'bounce'
+    });
+    gsap.from('.display-4', {
+      duration: 1,
+      x: -50,
+      opacity: 0,
+      delay: 1,
+      ease: 'back'
+    });
+  </script>
 
   <script>
     const scriptURL = 'https://script.google.com/macros/s/AKfycby3XrsmwPmxqV166SoRP2f3wnY_p9GM6Y_sFIQ9NfdJ-g2HITZsg3USVSwJxWiyv3dn/exec'
